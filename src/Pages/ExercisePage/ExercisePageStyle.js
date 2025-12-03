0578da5d0734c11e68e57229ee34268ab0954f1c
sebastian dalbjørn-winblad <zhadowseb@hotmail.com>
2025-12-03 19:43:50 +0100

    Adding in homepage with navigation bar to exercise page

---
+++ src/Pages/ExercisePage/ExercisePageStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#ccc",
+    marginBottom: 16,
+    paddingTop: 30,
+  },
+  loaderContainer: {
+    paddingVertical: 10,
+  },
+});

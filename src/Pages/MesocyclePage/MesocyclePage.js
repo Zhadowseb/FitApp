030b26056f6b7362ab4cde5d7db7b9449f12e501
Sebastian Dalbjørn <zhadowseb@hotmail.com>
2025-12-14 18:56:58 +0100

    cleaning page start

--- src/Pages/MesocyclePage/MesocyclePage.js
+++ src/Pages/MesocyclePage/MesocyclePage.js
@@ -1,26 +1,14 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button, Alert } from "react-native";
-import { useSQLiteContext } from "expo-sqlite";
+import { View } from "react-native";
 
 import styles from "./MesocyclePageStyle";
 
 const AddMesocycle = () => {
-  const db = useSQLiteContext();
-
-  const [program_name, set_Program_name] = useState("");
 
   return (
     <View style={styles.wrapper}>
-    
-        <TextInput
-            style={styles.input}
-            placeholder="Program Name"
-            value={program_name}
-            onChangeText={set_Program_name} />
 
-      <View style={styles.buttonContainer}>
-        <Button title="Add" onPress={handleSubmit} />
-      </View>
+
     </View>
   );
 };

f0c9bbc0f3ee83b5e8ebf9adc3ed68c7d46be991
Sebastian Dalbjørn <zhadowseb@hotmail.com>
2025-12-17 13:34:41 +0100

    base code added in

---
+++ src/Pages/MicrocyclePage/MicrocyclePage.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { View, Button, Text } from "react-native";
+import { useSQLiteContext } from "expo-sqlite";
+
+import styles from "./MicrocyclePageStyle";
+
+const MicrocyclePage = ( ) => {
+
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+};
+
+export default MicrocyclePage;

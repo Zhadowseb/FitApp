64cdeebfbc5cabc7100574de58698c37d399cdac
Sebastian Dalbjørn <zhadowseb@hotmail.com>
2025-12-22 03:53:15 +0100

    Can now edit and delete an EstimatedSet

--- src/Pages/ProgramOverviewPage/Components/rm_list/rm_list.js
+++ src/Pages/ProgramOverviewPage/Components/rm_list/rm_list.js
@@ -1,23 +1,26 @@
-// src/Components/ExerciseList/ExerciseList.js
 import { use, useState } from "react";
 import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { SQLiteDatabase, useSQLiteContext } from "expo-sqlite";
 import { useNavigation } from "@react-navigation/native";
 
 import styles from "./Rm_ListStyling"
+import EditEstimatedSet from "./Components/EditEstimatedSet/EditEstimatedSet";
 
 const rm_list = ( {program_id} ) => {
     const db = useSQLiteContext();
     const navigation = useNavigation();
 
     const [loading, setLoading] = useState(false);
+    const [editEstimatedSet_visible, set_editEstimatedSet_visible] = useState(false);
+
     const [estimated_sets, setEstimated_sets] = useState([]);
+    const [selectedSet, setSelectedSet] = useState(null);
 
     const loadEstimated_Sets = async () => {
         try{
             setLoading(true);
             const estimated_sets = await db.getAllAsync(
-                `SELECT estimated_weight, exercise_name FROM Estimated_Set WHERE program_id = ?;`,
+                `SELECT estimated_set_id, estimated_weight, exercise_name FROM Estimated_Set WHERE program_id = ?;`,
                 [program_id]
             );
             setEstimated_sets(estimated_sets);
@@ -28,11 +31,37 @@ const rm_list = ( {program_id} ) => {
         }
     }
 
+    const handleSubmit = async (data) => {
+        try{
+            await db.runAsync(
+                `UPDATE Estimated_set SET estimated_weight = ? WHERE estimated_set_id = ?;`,
+                [data.estimated_weight, data.id]
+            );
+
+        } catch (error) {
+            console.error(error);
+        } finally { setLoading(false); }
+    }
+
+    const handleDelete = async (data) => {
+        try{
+            await db.runAsync(
+                `DELETE FROM Estimated_set WHERE estimated_set_id = ?;`,
+                [data.id]
+            );
+
+        } catch (error) {
+            console.error(error);
+        } finally { setLoading(false); }
+    }
+
   const renderItem = ({ item }) => (
     <TouchableOpacity
-      style={styles.container}
-      onPress={() => {
-        navigation.navigate("HomePage")}}>
+        style={styles.container}
+        onPress={() => {
+            setSelectedSet(item);
+            set_editEstimatedSet_visible(true);
+        }}>
 
         <View style={styles.item_container}>
             <View style={styles.exercise_name}>
@@ -68,6 +97,13 @@ const rm_list = ( {program_id} ) => {
             }
             renderItem={renderItem}
         />
+
+        <EditEstimatedSet
+                visible = {editEstimatedSet_visible}
+                estimatedSet = {selectedSet}
+                onClose = {() => set_editEstimatedSet_visible(false)} 
+                onSubmit = {handleSubmit}
+                onDelete = {handleDelete} /> 
     </View>
   );
 };

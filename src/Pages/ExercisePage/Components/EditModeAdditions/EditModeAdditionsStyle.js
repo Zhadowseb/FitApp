1a8a1a2cd338a22306bcd3d0798fb20a41ee7d1a
Sebastian Dalbjørn <zhadowseb@hotmail.com>
2026-01-02 12:46:56 +0100

    Add in card styling for AddExercise again

---
+++ src/Pages/ExercisePage/Components/EditModeAdditions/EditModeAdditionsStyle.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+
+    //Main type styling.
+    card: {
+        backgroundColor: "#fff",
+        marginVertical: 10,
+        marginHorizontal: 10,
+        borderRadius: 10,
+        elevation: 3,
+        shadowColor: "#000",
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        shadowOffset: { height: 2, width: 0 },
+    },
+
+});

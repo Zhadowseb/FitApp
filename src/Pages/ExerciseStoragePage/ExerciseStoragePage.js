75e2088535d10b74a068d37e5fcec19eb4b08503
Sebastian Dalbjørn <zhadowseb@hotmail.com>
2026-01-13 00:19:41 +0100

    You can now add in an exercise to the library on the front page

--- src/Pages/ExerciseStoragePage/ExerciseStoragePage.js
+++ src/Pages/ExerciseStoragePage/ExerciseStoragePage.js
@@ -4,13 +4,19 @@ import { useNavigation } from '@react-navigation/native';
 import { useState, useEffect } from "react";
 import { useFocusEffect } from "@react-navigation/native";
 import { useCallback } from "react";
+import { useSQLiteContext } from "expo-sqlite";
 
 import styles from './ExerciseStoragePageStyle';
 import ExerciseStorageList from './Components/ExerciseStorageList/ExerciseStorageList';
+import AddExerciseStorageModal from "./Components/AddExerciseStorage/AddExerciseStorageModal"
 
 const ExerciseStoragePage = ( ) => {
 
+    const db = useSQLiteContext();
+
     const [refreshKey, set_refreshKey] = useState(0);
+    const [addExerciseStorage_visible, 
+        set_AddExerciseStorage_visible] = useState(false);
 
     const refresh = () => {
         set_refreshKey(prev => prev + 1);
@@ -22,6 +28,19 @@ const ExerciseStoragePage = ( ) => {
         }, [])
     );
 
+    const handleAdd = async (data) => {
+        try {
+            await db.runAsync(
+                `INSERT INTO Exercise_storage (exercise_name) VALUES (?);`,
+                [data.exercise_name]
+            );
+            refresh();
+            set_AddExerciseStorage_visible(false);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     
 
     return (
@@ -29,6 +48,16 @@ const ExerciseStoragePage = ( ) => {
         <View style={styles.container}>
             <ExerciseStorageList
                 refreshKey={refreshKey} />
+
+            <Button 
+                title="Add exercise" 
+                onPress={() => set_AddExerciseStorage_visible(true)}/>
+
+            <AddExerciseStorageModal 
+                visible={addExerciseStorage_visible}
+                onClose={() => set_AddExerciseStorage_visible(false)}
+                onSubmit={handleAdd}/>
+
 
         </View>
     );

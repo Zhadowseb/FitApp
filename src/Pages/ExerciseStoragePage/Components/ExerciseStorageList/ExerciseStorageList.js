de7c16ca3c9b4b26025d46a51cd0b3cb9f8db78f
Sebastian Dalbjørn <zhadowseb@hotmail.com>
2026-01-12 10:59:36 +0100

    Quick mistake fix

---
+++ src/Pages/ExerciseStoragePage/Components/ExerciseStorageList/ExerciseStorageList.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import styles from './ExerciseStoragePageStyle';
+
+const ExerciseStorageList = ( ) => {
+
+  return (
+
+    <View style={styles.container}>
+
+    </View>
+  );
+};
+
+export default ExerciseStorageList;

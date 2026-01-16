ddbdf502e760b378165c427ff50536a605d5337c
Sebastian Dalbjørn <zhadowseb@hotmail.com>
2026-01-16 18:45:18 +0100

    SetList title

--- src/Pages/ExercisePage/Components/ExerciseList/Components/ExerciseRow/SetList/Title/Title.js
+++ src/Pages/ExercisePage/Components/ExerciseList/Components/ExerciseRow/SetList/Title/Title.js
@@ -1,35 +1,40 @@
 import { View, Text, ScrollView } from "react-native";
 import styles from "./TitleStyle";
 
+
+import {ThemedCard,
+        ThemedText} 
+  from "../../../../../../../../Resources/Components";
+
 const Title = () => {
   return (
     <View style={styles.container}>
         <View style={[styles.pause, styles.text, styles.override]}> 
-        <Text> Pause </Text>
+        <ThemedText> Pause </ThemedText>
         </View>
 
         <View style={[styles.set, styles.text, styles.override]}> 
-        <Text> Set </Text>
+        <ThemedText> Set </ThemedText>
         </View>
 
         <View style={[styles.x, styles.text, styles.override]}> 
-        <Text> x </Text>
+        <ThemedText> x </ThemedText>
         </View>
 
         <View style={[styles.reps, styles.text, styles.override]}> 
-        <Text> Reps </Text>
+        <ThemedText> Reps </ThemedText>
         </View>
 
         <View style={[styles.rpe, styles.text, styles.override]}> 
-        <Text> RPE </Text>
+        <ThemedText> RPE </ThemedText>
         </View>
 
         <View style={[styles.weight, styles.text, styles.override]}> 
-        <Text> Weight </Text>
+        <ThemedText> Weight </ThemedText>
         </View>
 
         <View style={[styles.done, styles.text, styles.override]}> 
-        <Text> Done </Text>
+        <ThemedText> Done </ThemedText>
         </View>
     </View>
   );
